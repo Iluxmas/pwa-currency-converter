24d9f8cd63db48b33c65974b99bc9137e311daa3
Sulkhanov Ilya <masta.pxty@gmail.com>
2024-07-16 23:16:25 +0700

    fix: build errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
