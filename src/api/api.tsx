c3d2df152e6804a7cd9ec6e3bb2e2a98408b3af8
Sulkhanov Ilya <masta.pxty@gmail.com>
2024-07-16 23:02:26 +0700

    feat: api key to env

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { apikey } from './apikey';
 
 const BASE_URL = 'https://api.apilayer.com/fixer';
 // const BASE_URL = 'https://api.apilayer.com/exchangerates_data';
@@ -8,7 +7,7 @@ export const fetchCodes = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/symbols`, {
       headers: {
-        apikey: apikey,
+        apikey: import.meta.env.VITE_API_KEY,
         'Content-Type': 'application/json',
       },
     });
@@ -23,7 +22,7 @@ export const fetchRatio = async (base: string) => {
     const response = await axios.get(`${BASE_URL}/latest`, {
       params: { base },
       headers: {
-        apikey: apikey,
+        apikey: import.meta.env.VITE_API_KEY,
         'Content-Type': 'application/json',
       },
     });
